bab6168c2eccf587cb9f98abd99a7438b5962371
SAHIL SHAIKH <sahil.shaikh0514@gmail.com>
2023-09-13 13:26:13 +0530

    Added navbar and view product

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import Catalog from "../../features/catalog/Catalog";
 import Header from  "../layout/Header";
 import { Container, CssBaseline, createTheme } from "@mui/material";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 function App() {
   const [darkMode,setDarkMode]=useState(false);
   const paletteType=darkMode?'dark':'light';
@@ -22,7 +23,7 @@ function App() {
       <CssBaseline/>
       <Header darkMode={darkMode} onSwitchHandler={onSwitchHandler}/>
       <Container>
-       <Catalog/>
+        <Outlet/>
       </Container>
     </ThemeProvider>
   );
